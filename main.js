1d9ddabcae9d3e0b5ff34268b3ddb4cc1e328552
macintosh air <macintoshair@MacBook-Pro.local>
2023-02-06 17:01:43 -0500

    added comments

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+
+// increase the number of extra credit points (from 10) when the button is clicked
 let currCount = 10;
 
 function buttonClicked() {
@@ -15,21 +17,23 @@ function buttonClicked() {
 
 
 
-
+// create a function that will show and hide the surprise on click of the button
 function duckClick() {
-	console.log("duck button was clicked");
 
 	// select the element we want to modify
 	let duck = document.getElementById("duck");
 	let hidden = duck.getAttribute("hidden");
 
-
+	// hide image if it is showing, show image if it is hidden (ON CLICK)
 	if (hidden) {
 		duck.removeAttribute("hidden");
 		img.innerText = "hide image"
 	}
-	else{
+	else {
 		duck.setAttribute("hidden", "hidden");
 		img.innerText = "show image"
 	}
+
+	// report to console when duck button is clicked
+	console.log("duck button was clicked");
 }
